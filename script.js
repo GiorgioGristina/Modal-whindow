09cfe8f8bbf534efe2a4146168dc54cbef6c8a85
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-02-06 14:36:07 +0000

    added all event listener to open and close the window

--- script.js
+++ script.js
@@ -6,11 +6,27 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
  
+const openModal = () =>{
+  modal.classList.remove('hidden');
+    overlay.classList.remove('hidden')
+}
 
+const closeModal = () =>{
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', () => {
-    console.log('button clicked');
-  })
-  
+  btnsOpenModal[i].addEventListener('click', openModal );  
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener('click', closeModal);
+
+document.addEventListener ('keydown', (e) => {
+  console.log(e);
+  if (e.key  === 'Escape' && !modal.classList.contains('hidden')) {    
+      closeModal();    
+  }
+});
